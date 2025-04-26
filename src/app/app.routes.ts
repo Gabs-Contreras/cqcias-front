e0c85cf7c0d119f95f5f0862241751b85a1c4129
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-25 22:09:45 -0600

    "roots"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,27 @@
 import { Routes } from '@angular/router';
 import { ProductCrudComponent } from './components/product-crud/product-crud.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductViewComponent } from './components/product-view/product-view.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductCrudComponent
   },
+  {
+    path: 'todos',
+    component: ProductCrudComponent
+  },
+  {
+    path: 'nuevo',
+    component: ProductFormComponent
+  },
+  {
+    path: 'editar/:id',
+    component: ProductFormComponent
+  },
+  {
+    path: 'ver/:id',
+    component: ProductViewComponent
+  }
 ];
-
