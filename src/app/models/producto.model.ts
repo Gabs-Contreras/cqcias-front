984558904e80c94135ab463e3bac7b9d00ee7e63
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-25 22:32:18 -0600

    "modelo"

---
+++ src/app/models/producto.model.ts
@@ -0,0 +1,19 @@
+// src/app/models/product.model.ts
+
+export class Product {
+  id: number;
+  producto: string;
+  codigobarras: number;
+  marca: string;
+  costo: number;
+  estatus: string;
+
+  constructor(id: number, producto: string, codigobarras: number, marca: string, costo: number, estatus: string) {
+    this.id = id;
+    this.producto = producto;
+    this.codigobarras = codigobarras;
+    this.marca = marca;
+    this.costo = costo;
+    this.estatus = estatus;
+  }
+}
